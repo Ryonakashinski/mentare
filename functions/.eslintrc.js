a4b8f8b417bcd22cc51a0b844d704af754cdd4cd
Ryo Shimojo <ryo.bs.912@icloud.com>
2023-10-03 14:59:30 +0900

    fifth deploy

---
+++ functions/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  extends: ["eslint:recommended", "google"],
+  rules: {
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+    // Add these lines to disable 'no-unused-vars' for specific variables
+    "no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "onRequest|logger",
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
