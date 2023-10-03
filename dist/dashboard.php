f43f55296967a10bfb3ca8c5e1910691b9679a9d
Ryo Shimojo <ryo.bs.912@icloud.com>
2023-10-03 22:36:07 +0900

    eleven

--- dist/dashboard.php
+++ dist/dashboard.php
@@ -36,7 +36,7 @@ $user = $_SESSION['user'];
         <a href="logout.php" class="bg-gray-800 text-white px-6 py-2 rounded mt-6 inline-block">Logout</a><br>
 
         <!-- Add a link/button to navigate to index.map.php -->
-        <a href="index.html" class="bg-blue-500 text-white px-6 py-2 rounded mt-4 inline-block hover:bg-blue-700">Go to Map Page</a>
+        <a href="index.map.html" class="bg-blue-500 text-white px-6 py-2 rounded mt-4 inline-block hover:bg-blue-700">Go to Map Page</a>
     </div>
 
 </body>
